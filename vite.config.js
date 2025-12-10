80c22172e2c053efc12a54217a3c176b432f88da
Arnab Mondal <108365654+Arnab0610@users.noreply.github.com>
2025-12-10 20:58:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,9 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         // Niche tomar onno page-gulo add koro
-        about: resolve(__dirname, 'about.html'),
+        about: resolve(__dirname, 'about-us.html'),
         property: resolve(__dirname, 'property-listings.html'),
-        contact: resolve(__dirname, 'contact.html'), 
+        contact: resolve(__dirname, 'agents.html'), 
         // Joto gulo html file ache, sob ekhane comma diye likhte hobe
       },
     },
