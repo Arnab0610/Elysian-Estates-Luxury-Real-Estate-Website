10b99d7c153d819cee7f672f4c76e297699483d3
Arnab Mondal <108365654+Arnab0610@users.noreply.github.com>
2025-12-10 20:54:46 +0530

    uploade

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        // Niche tomar onno page-gulo add koro
+        about: resolve(__dirname, 'about.html'),
+        property: resolve(__dirname, 'property-listings.html'),
+        contact: resolve(__dirname, 'contact.html'), 
+        // Joto gulo html file ache, sob ekhane comma diye likhte hobe
+      },
+    },
+  },
+})
